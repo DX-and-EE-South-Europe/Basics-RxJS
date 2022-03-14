6ca04033de729eb8b0ceea40d69b8a2bea682297
CristinaRibateCog <Cristina.DelAlamo@cognizant.com>
2022-03-14 14:20:26 +0100

    style(): delete comments

--- src/app/components/dynamic-nav/dynamic-nav.component.ts
+++ src/app/components/dynamic-nav/dynamic-nav.component.ts
@@ -7,18 +7,4 @@ import { Component, Input } from '@angular/core';
 })
 export class DynamicNavComponent {
   @Input() navLabels!: any;
-
-  /* formatter(originLabel: string[] | SubComplexNav[]): any[] {
-    if (originLabel.length > 0) {
-      if (originLabel[0] instanceof Object) {
-        if (!this.isComplexNav) this.isComplexNav = true;
-        return (originLabel as SubComplexNav[]).map(({ labels }) =>
-          this.formatter(labels)
-        );
-      } else {
-        return createDataLinkArray(originLabel as string[], true);
-      }
-    }
-    return [];
-  } */
 }
