83713bad1a54df70d329082cc470eb55b8a2ee62
CristinaRibateCog <Cristina.DelAlamo@cognizant.com>
2022-03-24 13:43:39 +0100

    refactor(utils): function to arrow function

--- src/app/common/utils/selectDataPage.ts
+++ src/app/common/utils/selectDataPage.ts
@@ -1,7 +1,7 @@
 import { operatorsPages } from '../const/operators';
 import { DataPage } from '../interfaces';
 
-function selectFileDataPage(path: string[]): boolean | DataPage[] {
+const selectFileDataPage = (path: string[]): boolean | DataPage[] => {
   let objPage!: DataPage[];
   if (path.length == 1) return true;
   switch (path[0]) {
@@ -14,26 +14,25 @@ function selectFileDataPage(path: string[]): boolean | DataPage[] {
       console.error(`URL ERROR: no coincidences with ${path[0]}`);
   }
   return !!objPage ? objPage : false;
-}
+};
 
-function matchUrl(url: string): string[] {
+const matchUrl = (url: string): string[] => {
   const path = url.match(/[\w-]+/gi);
   return !!path ? path : [''];
-}
-function replacePath(path: string): string {
-  return path.replace(/-/gm, '');
-}
+};
 
-export function existDataPage(url: string): boolean {
+const replacePath = (path: string): string => path.replace(/-/gm, '');
+
+export const existDataPage = (url: string): boolean => {
   const path = matchUrl(url);
   const objPage: DataPage[] | boolean = selectFileDataPage(path);
 
   return typeof objPage === 'boolean'
     ? objPage
     : objPage.some(({ name }) => name.toLowerCase() === replacePath(path[1]));
-}
+};
 
-export function exportDataPage(url: string): DataPage | null {
+export const exportDataPage = (url: string): DataPage | null => {
   const path = matchUrl(url);
   const objPage: DataPage[] | boolean = selectFileDataPage(path);
   if (typeof objPage === 'object') {
@@ -48,4 +47,4 @@ export function exportDataPage(url: string): DataPage | null {
     return dataPage as DataPage;
   }
   return null;
-}
+};
